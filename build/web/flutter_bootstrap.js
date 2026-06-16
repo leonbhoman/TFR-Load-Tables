e7f9ece329ad6671052be30b80d28c9a31fdb237
leonbhoman <leonb.homan@gmail.com>
2026-06-16 22:49:33 +0200

    Manual release v1.0.6 - Restored pristine visual layouts. Cleared out main.dart.old from lib folder

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "553918801" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3694786850" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
